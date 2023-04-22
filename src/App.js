33480f29d08bbd304c77aeec8d437d3c93a9a1ba
pat0026 <patrick.caparros026@gmail.com>
2023-04-23 00:23:50 +0800

    [Chapter-5] Making API calls in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    "pending_items": [],
+    "done_items": [],
+    "pending_items_count": 0,
+    "done_items_count": 0
+  }
+
+  // makes the API call
+  getItems() {
+    axios.get("http://127.0.0.1:8000/v1/item/get",
+    {headers: {"token": "some_token"}})
+    .then(response => {
+      let pending_items = response.data["pending_items"]
+      let done_items = response.data["done_items"]
+
+      this.setState({
+        "pending_items": this.processItemValues(pending_items),
+        "done_items": this.processItemValues(done_items),
+        "pending_items_count": response.data["pending_item_count"],
+        "done_items_count": response.data["done_item_count"]
+      })
+    })
+  }
+
+  //ensures the API call is update when mounted
+  componentDidMount() {
+    this.getItems();
+  }
+
+  // convert items form API to HTML
+  processItemValues(items) {
+    let itemList = [];
+    items.forEach((item, index) => {
+      itemList.push(
+        <li key={index}>{item.title} {item.status}</li>
+      )
+    })
+    return itemList
+  }
+
+  //returns the HTML to be rendred
+  render() {
+    return (
+      <div className="App">
+        <h1>Done Items</h1>
+        <p>done item count: {this.state.done_items_count}</p>
+        {this.state.done_items}
+        <h1>Pending Items</h1>
+        <p> pending item count: {this.state.pending_items_count}</p>
+        {this.state.pending_items}
+      </div>
+    )
+  }
 }
 
 export default App;
